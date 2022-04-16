2e367c6d3f45b7c7fcde9114e00089e0feac258e
Layan0903 <andreamae.layan@evsu.edu.ph>
2022-04-17 01:42:22 +0800

    Activity 2

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+export class Tab3Page {
+
+  contactlist  = [
+    {id: 1, name: 'Andrea Mae Layan', email: 'bridgettearchimedes900@gmail.com',number: '09567653539'},
+    {id: 1, name: 'Sweet Revenge', email: 'Demon908@gmail.com',number: '09631852032'},
+    {id: 1, name: 'EcoreLov', email: 'Lovelicakes@gmail.com',number: '09062026630'},
+  ]
+
+}
